7d5d3e22b5ebc1361c19391ee1a9949f68328e0e
ilyakorchagin <hustlerilkor@gmail.com>
2025-12-03 17:36:37 +0300

    fix splash

--- composeApp/src/commonMain/kotlin/org/korchagin/kmp/activity/splash/fragments/screen/SplashScreen.kt
+++ composeApp/src/commonMain/kotlin/org/korchagin/kmp/activity/splash/fragments/screen/SplashScreen.kt
@@ -30,6 +30,7 @@ import org.korchagin.kmp.currentPlatform
 import team.platforma.extra_nav.navigator.activity.findNavHost
 import team.platforma.extra_nav.navigator.component.api.ComponentNavigator
 
+/*
 @OptIn(KoinExperimentalAPI::class)
 @Composable
 fun SplashScreen(componentNavigator: ComponentNavigator) {
@@ -82,6 +83,65 @@ fun SplashScreen(componentNavigator: ComponentNavigator) {
 
                 else -> {
                     GifImage(randomGifName)
+                }
+            }
+        }
+    }
+}
+*/
+@OptIn(KoinExperimentalAPI::class)
+@Composable
+fun SplashScreen(componentNavigator: ComponentNavigator) {
+    val authViewModel = koinViewModel<AuthViewModel>()
+    val mainViewModel = koinViewModel<MainViewModel>()
+    val currentUser by authViewModel.authState.collectAsState(null)
+
+    val visible = remember { mutableStateOf(true) }
+    val removed = remember { mutableStateOf(false) } // новое состояние
+
+    LaunchedEffect(Unit) {
+        delay(3000) // время показа GIF
+        visible.value = false // начинаем fadeOut
+        delay(1000) // ждём завершения анимации
+        removed.value = true // полностью убираем слой
+
+        if (currentUser != null) {
+            println("currentUser: ${currentUser!!.email}")
+            mainViewModel.loadCurrentUser(currentUser = currentUser!!.email!!)
+            delay(200)
+            val pupil = mainViewModel.currentPupil
+                .filterNotNull()
+                .first()
+
+            println("currentUser role: ${pupil.role}")
+
+            if (pupil.role == "admin")
+                findNavHost().navigateToActivitySingleTop(AdminActivity)
+            else
+                findNavHost().navigateToActivitySingleTop(MainActivity)
+
+        } else {
+            mainViewModel.loadCoachesList()
+            findNavHost().navigateToActivitySingleTop(AuthActivity)
+        }
+    }
+
+    val gifNames =
+        listOf("break_splash1", "break_splash2", "break_splash3", "break_splash4", "break_splash5")
+    val randomGifName = gifNames.random()
+    val gifUrl =
+        "https://firebasestorage.googleapis.com/v0/b/goodfootbreaking.appspot.com/o/Logo%2Fbreak_splash1.gif?alt=media&token=7086ff5c-a41e-4740-8358-f31a2d4c53b6"
+
+    // Показываем Box только если он ещё не удалён
+    if (!removed.value) {
+        AnimatedVisibility(
+            visible = visible.value,
+            exit = fadeOut(animationSpec = tween(durationMillis = 1000))
+        ) {
+            Box(modifier = Modifier.fillMaxSize().background(Color.White)) {
+                when (currentPlatform) {
+                    PlatformType.JS -> GifImage(drawable = gifUrl)
+                    else -> GifImage(randomGifName)
                 }
             }
         }
