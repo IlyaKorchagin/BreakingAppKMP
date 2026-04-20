47b703678cd387a02b26436916d5465cc36237fd
ilyakorchagin <hustlerilkor@gmail.com>
2026-04-20 23:15:05 +0300

    fix image resource

--- module_main/data_main/src/commonMain/kotlin/com/korchagin/data/di/MainDataKoinModule.kt
+++ module_main/data_main/src/commonMain/kotlin/com/korchagin/data/di/MainDataKoinModule.kt
@@ -3,14 +3,12 @@ package com.korchagin.data.di
 import com.korchagin.data.client.provideHttpClient
 import com.korchagin.data.repository.UserRepository
 import com.korchagin.data.repository.UserRepositotyImplementation
-import com.korchagin.data.yandexDiskRepository.YandexDiskRepository
-import com.korchagin.data.yandexDiskRepository.YandexDiskRepositotyImplementation
+
 import org.koin.dsl.bind
 import org.koin.dsl.module
 
 
 val mainDataRepositoryModule = module{
     single { UserRepositotyImplementation(get()) }.bind<UserRepository>()
-    single { YandexDiskRepositotyImplementation(get()) }.bind<YandexDiskRepository>()
     single { provideHttpClient() }
 }
